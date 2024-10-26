835478d9ec8f736ce03361a516b94aabc5687400
Mathis Logemann <mathisloge@gmail.com>
2024-10-26 15:34:46 +0200

    add needed dependencies

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,30 @@
+macro(declare_dependencies)
+    set(project_build_testing ${BUILD_TESTING})
+    set(BUILD_TESTING OFF)
+    set(STDEXEC_BUILD_EXAMPLES OFF)
+    FetchContent_Declare(
+        stdexec
+        GIT_REPOSITORY https://github.com/nvidia/stdexec.git
+        GIT_TAG        c4b905342c8335dca5f49617997b0997908a8111
+        FIND_PACKAGE_ARGS
+    )
+    FetchContent_Declare(
+        asio-grpc
+        GIT_REPOSITORY https://github.com/Tradias/asio-grpc.git
+        GIT_TAG        v3.2.1
+        FIND_PACKAGE_ARGS
+    )
+    FetchContent_MakeAvailable(stdexec asio-grpc)
+
+    set(BUILD_TESTING ${project_build_testing})
+    if(BUILD_TESTING)
+        set(CATCH_INSTALL_DOCS OFF)
+        FetchContent_Declare(
+            Catch2
+            GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+            GIT_TAG        v3.7.1
+            FIND_PACKAGE_ARGS
+        )
+        FetchContent_MakeAvailable(Catch2 )
+    endif()
+endmacro()
