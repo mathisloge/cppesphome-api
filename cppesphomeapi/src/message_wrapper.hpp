38bfe14c1d70586fff0c32f14ddafcc3f00dd803
Mathis Logemann <mathisloge@gmail.com>
2024-11-05 20:47:11 +0100

    add missing constructors and operator to message wrapper

--- cppesphomeapi/src/message_wrapper.hpp
+++ cppesphomeapi/src/message_wrapper.hpp
@@ -10,12 +10,43 @@ namespace cppesphomeapi
 class MessageWrapper
 {
   public:
+    MessageWrapper() = delete;
+    ~MessageWrapper() = default;
+
     template <typename TMsg>
         requires std::is_base_of_v<google::protobuf::Message, TMsg>
     MessageWrapper(std::shared_ptr<TMsg> message)
         : message_{std::move(message)}
         , message_id_{detail::get_message_id<TMsg>()}
     {}
+
+    MessageWrapper(MessageWrapper &&wrapper) noexcept
+        : message_{std::exchange(wrapper.message_, message_)}
+        , message_id_{std::exchange(wrapper.message_id_, message_id_)}
+    {}
+
+    MessageWrapper(const MessageWrapper &wrapper)
+        : message_{wrapper.message_}
+        , message_id_{wrapper.message_id_}
+    {}
+
+    MessageWrapper &operator=(MessageWrapper &&rhs) noexcept
+    {
+        message_ = std::exchange(rhs.message_, message_);
+        message_id_ = std::exchange(rhs.message_id_, message_id_);
+        return *this;
+    }
+
+    MessageWrapper &operator=(const MessageWrapper &rhs)
+    {
+        if (this == &rhs)
+        {
+            return *this;
+        }
+        message_ = rhs.message_;
+        message_id_ = rhs.message_id_;
+        return *this;
+    }
 
     template <typename TMsg>
         requires std::is_base_of_v<google::protobuf::Message, TMsg>
